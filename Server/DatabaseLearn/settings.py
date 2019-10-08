12787810190c889c155b122532d54a7769c023e1
kaushil24 <kaushil.kundalia@gmail.com>
2019-10-08 06:35:28 +0000

    added new user and made modifications to run django on server

--- Server/DatabaseLearn/settings.py
+++ Server/DatabaseLearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#o-1lc8@$a9lrdzg=k-uzza90s3&464j_5o$)(c+_6m$_y(p9k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.71.234.104']
 
 
 # Application definition
@@ -119,4 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
